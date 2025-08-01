8d3ad13ac510e66d65cb01554125055410b651e0
sawankumar1 <89899480+sawankumar1@users.noreply.github.com>
2025-08-01 11:55:45 +0530

    detail

---
+++ Postgres/RestApiWeightBridgeMechine/ViewController/src/com/bspl/bean/VechileSubCodeDetails.java
@@ -0,0 +1,22 @@
+package com.bspl.bean;
+
+public class VechileSubCodeDetails {
+    private String code;
+    private String subtype_desc;
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setSubtype_desc(String subtype_desc) {
+        this.subtype_desc = subtype_desc;
+    }
+
+    public String getSubtype_desc() {
+        return subtype_desc;
+    }
+}
